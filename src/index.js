bfc6cd370818c3d5739bb2bb1a95707f624ec3ad
alya98 <alya-1998@list.ru>
2018-02-27 19:52:02 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,4 +2,34 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    var money = {}, tmp;
+    if (currency > 10000) {
+    money.error = "You are rich, my friend! We don't have so much coins for exchange";
+        return money;
+    }
+    if (currency / 50 >= 1) {
+        money.H = Math.floor(currency / 50);
+
+    }
+    tmp = currency % 50;
+    if (tmp / 25 >= 1) {
+        money.Q = Math.floor(tmp / 25);
+
+    }
+    tmp = tmp % 25;
+    if (tmp / 10 >= 1) {
+        money.D = Math.floor(tmp / 10);
+
+    }
+    tmp = tmp % 10;
+    if (tmp / 5 >= 1) {
+        money.N = Math.floor(tmp / 5)
+    }
+    tmp = tmp % 5;
+    if (tmp !== 0) money.P = tmp;
+
+    return money;
 }
+       // console.log(makeExchange(44));  
+
+
